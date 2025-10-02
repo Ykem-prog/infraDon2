f17e1d60c951a9ea2d864207b2b8a40af300a97e
Ykem-prog <valentin.ferrini@heig-vd.chgit config --global user.emaillgit config --global user.email valentin.ferrini@heig-vd.ch>
2025-10-02 15:40:29 +0200

    Ini vue

---
+++ vue-infradon2/src/components/TheWelcome.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+//model
+const counter = ref(0)
+
+const increment = () => {
+  counter.value++
+}
+</script>
+
+<template>
+  <h1>Célibataire voici mon numéro : 079 735 11 66</h1>
+  <p>{{ counter }}</p>
+  <button @click="increment">Ajouter 1</button>
+</template>
