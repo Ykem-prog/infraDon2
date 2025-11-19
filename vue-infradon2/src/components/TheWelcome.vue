14eb28667724a902e4eea05845824ebfa456998f
Ykem-prog <valentin.ferrini@heig-vd.chgit config --global user.emaillgit config --global user.email valentin.ferrini@heig-vd.ch>
2025-11-19 21:17:23 +0100

    initialisation de la local base + creation de transfert entre local et online

--- vue-infradon2/src/components/TheWelcome.vue
+++ vue-infradon2/src/components/TheWelcome.vue
@@ -1,143 +1,267 @@
 <script setup lang="ts">
-import { onMounted, ref, type Ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import PouchDB from 'pouchdb'
+import PouchDBFind from 'pouchdb-find'
+
+PouchDB.plugin(PouchDBFind)
 
 declare interface Post {
+  _conflicts: null
+  _id: string
+  _rev: string
   title: string
-  post_content: string
+  content: string
   attributes: {
     creation_date: any
   }
 }
 
-// Référence à la base de données
 const storage = ref()
-// Données stockées
 const postsData = ref<Post[]>([])
-
-// Initialisation de la base de données
-const initDatabase = () => {
-  console.log('=> Connexion à la base de données')
-
-  const db = new PouchDB('http://ykem:F4bd3df077e@localhost:5984/infradon2_db')
-  if (db) {
-    console.log('Connecté à la collection : ' + db?.name)
-    storage.value = db
-  } else {
-    console.warn('Echec lors de la connexion à la base de données')
-  }
-}
-
-// Récupération des données
-
-// TODO Récupération des données
-// https://pouchdb.com/api.html#batch_fetch
-// Regarder l'exemple avec function allDocs
-// Remplir le tableau postsData avec les données récupérées
-const titre = ref('')
-const actor = ref('')
-const content = ref('')
-const id: Ref<string> = ref('')
-const addData = () =>
-  storage.value
-    .post({
-      title: titre.value,
-      post_content: content.value,
-      actor: actor.value,
-    })
-    .then(function (response) {
-      console.log(response)
-      fetchData()
-      titre.value = ''
-      actor.value = ''
-      content.value = ''
-      // handle response
-    })
-    .catch(function (err) {
-      console.log(err)
-    })
-const fetchData = (): any => {
-  storage.value
-    .allDocs({ include_docs: true })
-    .then((result: any) => {
-      console.log('=> Données récupérées :', result.rows)
-      postsData.value = result.rows.map((row: any) => row.doc)
-      console.log(postsData.value)
-    })
-    .catch((error: any) => {
-      console.error('=> Erreur lors de la récupération des données :', error)
-    })
-}
+const sync = ref()
 
 onMounted(() => {
   console.log('=> Composant initialisé')
   initDatabase()
-  fetchData()
 })
-const getData = (id: string) => {
+
+const initDatabase = () => {
+  console.log('=> Connexion à la base de données')
+  const localdb = new PouchDB('collection_infradon2')
+  if (localdb) {
+    console.log('Connected to collection : ' + localdb?.name)
+    storage.value = localdb
+    storage.value
+      .createIndex({
+        index: {
+          fields: ['content'],
+        },
+      })
+      .then(console.log('index created'))
+    localdb.replicate
+      .from('http://ykem:F4bd3df077e@localhost:5984/infradon2')
+      .on('complete', syncData)
+      .then((_result) => {
+        fetchData()
+      })
+  } else {
+    console.warn('Something went wrong')
+  }
+}
+
+const syncData = () => {
+  sync.value = storage.value
+    .sync('http://ykem:F4bd3df077e@localhost:5984/infradon2', { live: true, retry: true })
+    .on('change', fetchData)
+}
+
+const toggle = () => {
+  if (sync.value) {
+    sync.value.cancel()
+    sync.value = null
+  } else {
+    syncData()
+  }
+}
+
+const search = (event: Event) => {
+  event.target.blur()
+
+  if (event.target.value === '') {
+    fetchData()
+  } else {
+    storage.value
+      .find({
+        selector: { content: `Contenu du document : ${event.target.value}` },
+      })
+      .then((result: any) => {
+        console.log('=> Données récupérées :', result.docs)
+        postsData.value = result.docs
+        // console.log(postsData)
+      })
+      .catch((error: any) => {
+        console.error('Erreur lors de la récupération des données :', error)
+      })
+  }
+}
+
+const searchReset = () => {
+  document.querySelector('.search').value = ''
+  fetchData()
+}
+
+const fetchData = (): any => {
   storage.value
-    .get(id)
-    .then((doc: any) => {
-      titre.value = doc.title
-      content.value = doc.post_content
-      actor.value = doc.actor
-      id.value = doc._id
-      return storage.value.put(doc)
+    .allDocs({
+      include_docs: true,
+      conflicts: true,
     })
-    .then((response: any) => {
-      console.log('Document modifié', response)
-      fetchData()
+    .then((result: any) => {
+      console.log('=> Données récupérées :', result.rows)
+      postsData.value = result.rows.map((row: any) => row.doc)
+      // console.log(postsData)
+    })
+    .catch((error: any) => {
+      console.error('Erreur lors de la récupération des données :', error)
     })
 }
-const deleteData = (id: string) => {
+
+const createDoc = (): any => {
+  counter++
   storage.value
-    .get(id)
-    .then((doc: any) => {
-      return storage.value.remove(doc)
+    .post({
+      title: 'Document ' + counter,
+      content: 'Contenu du document : ' + words[Math.floor(Math.random() * words.length)],
     })
-    .then((response: any) => {
-      console.log('Document supprimé', response)
+    .then(function (response: any) {
       fetchData()
+      console.log(response)
+    })
+    .catch(function (err: any) {
+      console.log(err)
     })
 }
-const modifiedData = (id: string) => {
+
+const deleteDoc = (post: Post): any => {
   storage.value
-    .get(id)
-    .then((doc: any) => {
-      doc.title = titre.value
-      doc.post_content = content.value
-      doc.actor = actor.value
-      return storage.value.put(doc)
-    })
-    .then((response: any) => {
-      console.log('Document modifié', response)
+    .remove(post)
+    .then(function (response: any) {
       fetchData()
+      console.log(response)
+    })
+    .catch(function (err: any) {
+      console.log(err)
     })
 }
-//v-model permet de lier les champs du formulaire aux variables du script
+
+const updateDoc = (post: Post): any => {
+  post.content = 'Contenu du document : ' + words[Math.floor(Math.random() * words.length)]
+  storage.value
+    .put(post)
+    .then(function (response: any) {
+      fetchData()
+      console.log(response)
+    })
+    .catch(function (err: any) {
+      console.log(err)
+    })
+}
+
+let counter = 0
+const words = [
+  'léa',
+  'inoé',
+  'camilo',
+  'teicir',
+  'yannis',
+  'elia',
+  'loann',
+  'sasita',
+  'sarah',
+  'enya',
+  'gabriel',
+  'nuno',
+  'tanguy',
+  'loic',
+  'dylan',
+  'liliana',
+  'thierry',
+  'valentin',
+  'benoît',
+  'chloé',
+]
 </script>
 
 <template>
   <h1>Fetch Data</h1>
-  <button @click="addData">Ajouter Document</button>
-  <button @click="modifiedData(id)">Modifier</button>
-
-  <form action="" @submit="addData">
-    <label for="titre">Titre:</label><br />
-    <input type="text" id="titre" name="titre" v-model="titre" /><br />
-    <label for="actorName">Acteur:</label><br />
-    <input type="text" id="actorName" name="lname" v-model="actor" />
-    <br />
-    <label for="content">Contenu:</label><br />
-    <input type="text" id="content" name="content" v-model="content" /><br />
-  </form>
-
+  <label class="switch">
+    <input type="checkbox" checked @click="toggle" /><span class="slider round"></span>
+  </label>
+  <label v-if="sync"> Online</label>
+  <label v-else> Offline</label>
+  <br /><br />
+  <input type="text" placeholder="Search" @keyup.enter="search" class="search" />
+  <button @click="searchReset">X</button>
+  <br /><br />
+  <button @click="createDoc">Ajouter un document</button>
   <article v-for="post in postsData" v-bind:key="(post as any).id">
-    <h2>{{ post.title }}</h2>
-    <h2>{{ post.post_content }}</h2>
-    <h2>{{ post.actor }}</h2>
-
-    <button @click="getData(post._id)">Get Data</button>
-    <button @click="deleteData(post._id)">Delete Data</button>
+    <br />
+    <h2>
+      {{ post.title }}<span class="conflicts" v-if="post._conflicts">Attention, conflits !</span>
+    </h2>
+    <p>{{ post.content }}</p>
+    <button @click="updateDoc(post)">Update</button>
+    <button @click="deleteDoc(post)">Effacer</button>
   </article>
 </template>
+
+<style scoped>
+.conflicts {
+  color: rgb(140, 3, 3);
+  font-style: italic;
+  font-size: small;
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 30px;
+  height: 17px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: '';
+  height: 13px;
+  width: 13px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #2196f3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(13px);
+  -ms-transform: translateX(13px);
+  transform: translateX(13px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 17px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
